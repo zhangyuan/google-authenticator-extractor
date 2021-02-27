70647cf3fc01d42f4edceaae153dea2324f4f25c
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-02-28 00:15:33 +0800

    fix apt-get permission issue

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -3,8 +3,8 @@
 
 set -eou pipefail
 
-apt-get update
-apt-get install -y build-essential mingw-w64
+sudo apt-get update
+sudo apt-get install -y protobuf-compiler build-essential mingw-w64
 
 rustup toolchain install stable-x86_64-pc-windows-gnu
 rustup target add x86_64-pc-windows-gnu
