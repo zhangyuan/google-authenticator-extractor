5961e46ab28a5286a44ac5727826dc3caee10cd9
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-02-28 00:22:04 +0800

    cross build for x86_64-apple-darwin

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -12,3 +12,6 @@ rustup target add x86_64-pc-windows-gnu
 rustup toolchain install stable-x86_64-unknown-linux-gnu
 rustup target add x86_64-unknown-linux-gnu
 
+rustup toolchain install stable-x86_64-apple-darwin
+rustup target add x86_64-apple-darwin
+
