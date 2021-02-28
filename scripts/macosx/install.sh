5b5da2af68b8f8c0765454ea8117f77fae906417
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-02-28 13:08:37 +0800

    install target on macos

--- scripts/macosx/install.sh
+++ scripts/macosx/install.sh
@@ -6,7 +6,11 @@ brew install protobuf
 
 # for target x86_64-pc-windows-gnu
 brew install mingw-w64
+rustup toolchain install stable-x86_64-pc-windows-gnu
+rustup target add x86_64-pc-windows-gnu
 
 # for target x86_64-unknown-linux-gnu
 brew tap SergioBenitez/osxct
 brew install x86_64-unknown-linux-gnu
+rustup toolchain install stable-x86_64-unknown-linux-gnu
+rustup target add x86_64-unknown-linux-gnu
--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -11,7 +11,3 @@ rustup target add x86_64-pc-windows-gnu
 
 rustup toolchain install stable-x86_64-unknown-linux-gnu
 rustup target add x86_64-unknown-linux-gnu
-
-rustup toolchain install stable-x86_64-apple-darwin
-rustup target add x86_64-apple-darwin
-
