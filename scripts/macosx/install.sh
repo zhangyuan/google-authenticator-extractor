1db17d950cdc90d2d8c685e6787fa005de03c336
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-02-28 13:04:59 +0800

    build for x86_64-unknown-linux-gnu on macos

--- scripts/macosx/install.sh
+++ scripts/macosx/install.sh
@@ -6,3 +6,7 @@ brew install protobuf
 
 # for target x86_64-pc-windows-gnu
 brew install mingw-w64
+
+# for target x86_64-unknown-linux-gnu
+brew tap SergioBenitez/osxct
+brew install x86_64-unknown-linux-gnu
