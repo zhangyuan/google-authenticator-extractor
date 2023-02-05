02915b98147e76ec1524404b4dd968d047772403
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-02-06 00:50:15 +0800

    use arduino/setup-protoc@v1 (#96)

--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -3,10 +3,8 @@
 
 set -eou pipefail
 
-rustup component add clippy
-
 sudo apt-get update
-sudo apt-get install -y protobuf-compiler build-essential mingw-w64
+sudo apt-get install -y mingw-w64
 
 rustup toolchain install stable-x86_64-pc-windows-gnu
 rustup target add x86_64-pc-windows-gnu
